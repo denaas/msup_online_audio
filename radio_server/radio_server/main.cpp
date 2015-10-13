bf88287bd9282b05cab5573111a85da5f2c1da2e
Kirill Voev <voev.kirill@gmail.com>
2015-10-14 02:14:48 +0300

    Вроде последний коммит на сегодня

---
+++ radio_server/radio_server/main.cpp
@@ -0,0 +1,33 @@
+#include "header/serv.h"
+
+int main()
+{
+	try
+	{
+		WSADATA WsaData;
+		// Initialization Winsock
+		InitializationWSA(WsaData);
+		
+		// Create a listening socket
+		ServerSocket socket(TCP);	
+		
+		// Bind the socket to the local IP address and port 8080
+		socket.Bind();
+		socket.Listen(20);
+
+		while (1) 
+		{
+			BaseSocket * feedback_socket = socket.Accept(TCP); // Creating a new socket for feedback
+			socket.OnAccept(feedback_socket); // Answer on request
+		}
+
+		// Disable Winsock
+		if (WSACleanup())
+			throw string("cleaning of WSA");
+		return 0;
+	}
+	catch (string s)
+	{
+		cout << "Error: " << s << endl;
+	}
+}
