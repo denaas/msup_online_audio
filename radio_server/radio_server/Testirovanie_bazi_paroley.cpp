cab9f0fc4854b946b027e35ae5848c44518dbf17
SkorMaxx <skm1000@mail.ru>
2015-12-15 17:52:40 +0300

    Primer ispolzovaniyas s poyasneniyami

--- radio_server/radio_server/Testirovanie_bazi_paroley.cpp
+++ radio_server/radio_server/Testirovanie_bazi_paroley.cpp
@@ -1,15 +1,21 @@
+//podkluchit k osnovnoy programme
 #include <iostream>
 #include <cstdio>
 #include <cstddef>
 #include "sha_256.cpp"
 #include <string>
 #include "registr.cpp"
+//
+
 
 using namespace std;
 
 int main()
 {
+    // sdelat takuyu peremennuyu
     SHA256* YourName = new SHA256();
+    //
+
     string name;
     string password;
 
@@ -20,8 +26,11 @@ int main()
     identification man;
     man.login = name;
     man.password = password;
+
+    // process heshirovaniya
     man.login = YourName->hash(name);
     man.password = YourName->hash(password);
+    //
 
     cout << "1 - registration" << endl;
     cout << "2 - autorization" << endl;
@@ -33,14 +42,20 @@ int main()
     {
         case 1:
             {
+            // Proverka vozmozhnosti registracii
+            // Pri vozvrate 0 - uspeh
             if (man.registration() != 0) cout << "write different login" << endl;
+            //
             else cout << "vnesen" << endl;
             main();
             break;
             }
         case 2:
             {
+            // Proverka avtorizacii
+            // Pri vozvrate 0 - uspeh
             if (man.autorization() == 1) cout << "error login or password" << endl;
+            //
             else
                 cout << "Here" << endl;
             main();
