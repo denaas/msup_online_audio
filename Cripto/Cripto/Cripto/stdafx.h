c9c68afc204bc885418fc8946efa60e6d0dc1513
RGV <grl.v@ya.ru>
2015-12-23 09:33:44 +0300

    Криптобиблиотека. Кинул весьь проект на всякий. В Criptofunction.h описание. Dll-ка

---
+++ Cripto/Cripto/Cripto/stdafx.h
@@ -0,0 +1,32 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // »сключите редко используемые компоненты из заголовков Windows
+// ‘айлы заголовков Windows:
+#include <windows.h>
+
+#pragma warning(push)
+#pragma warning(disable : 4820)
+#pragma warning(disable : 4619)
+#pragma warning(disable : 4548)
+#pragma warning(disable : 4668)
+#pragma warning(disable : 4365)
+#pragma warning(disable : 4710)
+#pragma warning(disable : 4371)
+#pragma warning(disable : 4826)
+#pragma warning(disable : 4061)
+#pragma warning(disable : 4640)
+#include <stdio.h>
+#include <memory.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
